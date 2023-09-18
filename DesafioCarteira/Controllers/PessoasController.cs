2088d85e86fb4cd7b8be31a5e1aec6ace8eb012d
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-18 14:05:41 -0300

    Created Pessoas Controller

---
+++ DesafioCarteira/Controllers/PessoasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesafioCarteira.Services;
+
+namespace DesafioCarteira.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly PessoasService _pessoasService;
+
+        public PessoasController(PessoasService pessoasService) => _pessoasService = pessoasService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Find()
+        {
+            var pessoas = await _pessoasService.FindAll();
+            return Json(pessoas);
+        }
+    }
+}
