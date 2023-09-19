b2986083c39a982c10873917f22912d88d52854c
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-19 14:29:55 -0300

    Confirm Modal

---
+++ DesafioCarteira/wwwroot/js/viewmodels/ConfirmModalVM.js
@@ -0,0 +1,8 @@
+﻿var ConfirmModal = new function ConfirmModalVM() {
+    var self = this;
+    self.ModalAction = ko.observable("");
+    self.ModalButton = ko.observable("");
+    self.ModalTitle = ko.observable();
+}
+
+ko.applyBindings(ConfirmModal, document.getElementById('confirm-modal'));
