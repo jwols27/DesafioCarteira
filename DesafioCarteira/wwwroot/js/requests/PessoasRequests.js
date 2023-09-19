675615524fcd057af690c280bbe7b51673111081
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-19 14:30:17 -0300

    Merged Pessoas Requests js files

---
+++ DesafioCarteira/wwwroot/js/requests/PessoasRequests.js
@@ -0,0 +1,26 @@
+﻿function getPessoas(callback){
+    $.ajax({
+        url: '/Pessoas/Find',
+        type: 'GET',
+        success: function (data) {
+            callback(data);
+        },
+        error: function () {
+            console.log('Error fetching data.');
+        }
+    });
+}
+
+function postPessoa(pessoa){
+    $.ajax({
+        url: '/Pessoas/Create',
+        type: 'POST',
+        data: { pessoa: JSON.stringify(pessoa) },
+        success: function (data) {
+            console.log(data);
+        },
+        error: function () {
+            console.log('Error posting data.');
+        }
+    });
+}
