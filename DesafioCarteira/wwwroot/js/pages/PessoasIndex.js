a567c386cbc41d77c39658849549b06f9af15abd
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-19 14:31:35 -0300

    Finished Pessoas Index

---
+++ DesafioCarteira/wwwroot/js/pages/PessoasIndex.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    getPessoas(PessoasIndex.Pessoas);
+});
---
+++ DesafioCarteira/wwwroot/js/viewmodels/PessoasIndexVM.js
@@ -0,0 +1,13 @@
+﻿var PessoasIndex = new function PessoasIndexVM() {
+    var self = this;
+    self.formatNumber = function (number) {
+        if (!number) return "-";
+        return "R$" + number.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
+
+    self.teste = ko.observable(2);
+
+    self.Pessoas = ko.observableArray([]);
+}
+
+ko.applyBindings(PessoasIndex, document.getElementById('pessoas-view'));
