ee50fcf7643876248121cb0192071f335c192181
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-19 16:20:19 -0300

    Pessoa Details View

---
+++ DesafioCarteira/wwwroot/js/pages/PessoaDetails.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    var text = $("#pessoa-id").text();
+
+    ConfirmModal.ModalAction("deletar esta pessoa?");
+    ConfirmModal.ModalButton("btn-deletar-pessoa");
+    ConfirmModal.ModalTitle("Deletar");
+
+    $("#btn-deletar-pessoa").click(() => {
+        deletePessoa(parseInt(text.trim()))
+            .then((res) => {
+                if (res.success) {
+                    window.location.href = res.redirectUrl;
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+    })
+});
